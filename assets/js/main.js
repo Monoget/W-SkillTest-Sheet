cec5b484722c0a2b843d6493cf3c789347db362a
Monoget Saha <monoget2@gmail.com>
2024-09-06 21:40:00 +0600

    update all

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,13 @@ document.getElementById('nextBtn-1').addEventListener('click', function () {
     document.getElementById('section-2').classList.add('active');
 });
 
-document.getElementById('nextBtn-2').addEventListener('click', function () {
+document.getElementById('submit').addEventListener('click', function () {
     document.getElementById('section-2').classList.remove('active');
+    document.getElementById('section-3').classList.add('active');
     // Move to the next section accordingly
+});
+
+document.getElementById('clearBtn').addEventListener('click', function() {
+    // Reset all input fields
+    document.getElementById('multiStepForm').reset();
 });
